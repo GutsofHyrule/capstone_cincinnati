71881ede1bbbc400f801b72a158a2e135a782a2a
Spencer <dresmanns@gmail.com>
2022-06-01 10:05:59 -0400

    update

--- src/Routes/authRoutes.js
+++ src/Routes/authRoutes.js
@@ -7,6 +7,10 @@ router.get('/places', authControllers.places_get)
 router.post('/places', authControllers.places_post)
 router.get('/food', authControllers.food_get)
 router.post('/food', authControllers.food_post)
+router.get('/signup', authControllers.signup_get)
+router.post('/signup', authControllers.signup_post)
+router.get('/login', authControllers.login_get)
+router.post('/login', authControllers.login_post)
 
 module.exports= router
 
--- src/controllers/authControllers.js
+++ src/controllers/authControllers.js
@@ -1,8 +1,26 @@
 const jwt = require('jsonwebtoken')
+const User = require('../models/comments')
 
-const handleErrors = (err) =>{
-    console.log(err.message, err.code)
-    let errors = {comment: ''}
+const handleErrors = (err, comerr ) =>{
+    console.log(comerr.message, comerr.code)
+    let comErrors = {comment: ''}
+    let errors = {email: '', password: ''}
+    if (err.message === 'incorrect email'){
+    errors.email =  'That email is not registered'
+}
+if(err.message === 'incorrect password'){
+    errors.password = 'that password did not work'
+}
+if(errors.code === 11000){
+    errors.email = 'that email is in use'
+    return errors
+}
+if(err.message.includes('user validation failed')){
+    Object.values(err.errors).forEach(({properties}) => {
+        errors[properties.path] = properties.message
+    })
+}
+return errors
 }
 
 const maxAge = 3*24*60*60
@@ -13,4 +31,36 @@ const createToken = (id) =>{
     })
 }
 
- 
+module.exports.signup_get = (req,res) => {
+    res.render('signup')
+}
+module.exports.signup_get = (req,res) => {
+    res.render('login')
+}
+
+module.exports.signup_post = async(req,res) =>{
+    const{email, password} = req.body
+    try{
+    const user = await User.create({email, password})
+    const token = createToken(user._id)
+    res.cookie('jwt', token, {httpOnly: true, maxAge: maxAge * 1000})
+    }
+    catch(err){
+        const errors = handleErrors(err)
+        res.status(400).json({errors})
+    }
+}
+module.exports.login_post = async(req,res) => {
+    const {email, password} = req.body
+
+    try{
+        const user = await User.login(email,password)
+        const token = createToken(user._id)
+        res.cookie('jwt', token, {httpOnly: true, maxAge: maxAge * 1000})
+        res.status(200).json({user: user._id})
+    }
+    catch(err){
+        const errors = handleErrors(err)
+        res.status(400).json({errors})
+    }
+}
