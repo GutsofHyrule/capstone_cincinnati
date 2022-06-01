64ac3cdb0f1e373ff87c1d48e24a94b0523527e4
Spencer <dresmanns@gmail.com>
2022-06-01 16:23:23 -0400

    im so happy

--- server.js
+++ server.js
@@ -1,35 +1,19 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const authRoutes = require('./Routes/authRoutes');
 require('dotenv').config();
-
 const app = express();
 const port = process.env.PORT || 5000;
-
 app.use(express.json());
-
-
 const uri = process.env.ATLAS_URI;
-
-
 mongoose.connect(uri, { useUnifiedTopology: true ,  useNewUrlParser: true }
   );
   const connection = mongoose.connection;
-  
-  
   connection.once('open', () => {
     console.log("MongoDB is connected");
   })
-
-  const usersRouter = require('./Routes/user');
-
-  app.get('/', (req, res) => res.render('home'));
-  app.get('/user', usersRouter);
-  app.use(authRoutes);
-
+  const usersRouter = require('./routes/users');
+  app.use('/user', usersRouter);
   app.listen(port, () =>{
     console.log(`server is running on port: ${port}`);
 });
-
-
-// Here is the magic comment 
+// Here is the magic comment
