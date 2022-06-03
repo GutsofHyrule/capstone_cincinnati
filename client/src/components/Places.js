d22270a902750df3f0fb3ad064823c191259f3c9
Spencer <dresmanns@gmail.com>
2022-06-03 10:19:31 -0400

    ballin

---
+++ client/src/components/Places.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "./style.spence.css"
+
+const Places= () => {
+    
+        return(
+            <div className="container">
+                <h4 className = "center">Places</h4>
+                <h5>Explore the west.</h5>
+            </div>
+        )
+    }
+    export default(Places)
