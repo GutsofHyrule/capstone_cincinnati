46db04544466168386b9141d84737759e2e70ab9
Spencer <dresmanns@gmail.com>
2022-06-07 13:21:33 -0400

    pull

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -1,41 +1,39 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="style.Karim.css">
-  <script src="https://kit.fontawesome.com/3a32ef72e8.js" crossorigin="anonymous"></script>
-  <title>Contact</title>
-</head>
-<body>
-  <div class="hero">
+import React from 'react'
+import "./style.Karim.css"
+
+const Contact = () =>{
+
+  return(
+<div className= 'container-fluid p-0'>
+  <div className="hero">
     <form>
-      <div class="row">
-        <div class="input-group">
-          <input type="text" id= "name" required>
-          <label for=""><i class="fa-solid fa-user"></i>Your Name</label>
+      <div className="row">
+        <div className="input-group">
+          <input type="text" id= "name" required />
+          <label className='text-title' for=""><i className="fa-solid fa-user"></i>Your Name</label>
           </div>
     
-          <div class="input-group">
-            <input type="text" id= "number" required>
-            <label for=""><i class="fa-solid fa-phone"></i>Phone number</label>
+          <div className="input-group">
+            <input type="text" id= "number" required />
+            <label className='text-title' for=""><i className="fa-solid fa-phone"></i>Phone number</label>
             </div>
       </div>
 
-        <div class="input-group">
-          <input type="text" id= "email" required>
-          <label for=""><i class="fa-solid fa-envelope"></i>Email</label>
+        <div className="input-group">
+          <input type="text" id= "email" required />
+          <label className='text-title' for=""><i className="fa-solid fa-envelope"></i>Email</label>
           </div>
 
-          <div class="input-group">
-            <textarea id="message" rows="8" required></textarea>
-            <label for=""><i class="fa-solid fa-message"></i>Your Message</label>
+          <div className="input-group">
+            <textarea id="message" rows="20" required></textarea>
+            <label className='text-title' for=""><i className="fa-solid fa-message"></i>Your Message</label>
             </div>
 
         <button type="submit">Submit</button>
     </form>
   </div>
+</div>)}
+
+export default( Contact)
   
-</body>
-</html>
+
