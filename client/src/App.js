18bc64abf998f73902a39cf295f2c705753d940e
habil tabesh <habil.tabesh134@gmail.com>
2022-06-13 14:02:02 -0400

    FAQ

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import Events from './components/Events'
 import Home from './components/Home'
 import Footer from './components/Footer'  
 import Contact from './components/Contact'
+import FAQs from './components/FAQ'
 class App extends Component{
   render(){
     return(
@@ -23,6 +24,7 @@ class App extends Component{
             <Route path='/amusements' component = {Places} />
             <Route path='/events' component={Events} />
             <Route path='/contact' component={Contact} />
+            <Route path='/FAQs' component={FAQs} />
           </Switch>
           </div>
           <Footer/>
