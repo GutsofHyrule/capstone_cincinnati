71cb770eccb71bc73e67a15c2ba6cd278e7bd3a2
habil tabesh <habil.tabesh134@gmail.com>
2022-06-13 09:16:32 -0400

    pull

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -14,10 +14,20 @@ const Home = () => {
     
 <MyVideoComponent/>
     <div className="container-fluid"><img className='homeImg'  src={myImage} alt=""/></div>
-        
+    <div className="container-fluid">
+    <img className='welcome '  src={welcome} alt="" srcset=""/>
+    <h2 className="welcome-text">Welcome </h2>
+    <h2 className="to-text">to</h2>
+    </div>
     <div id="3fb7eb4b8c29bbe54ec5c070c0983fd1" className="ww-informers-box-854753"><p className="ww-informers-box-854754"><a href="https://world-weather.info/forecast/usa/cincinnati/14days/">world-weather.info/forecast/usa/cincinnati/14days/</a><br /><a href="https://world-weather.info/">world-weather.info</a></p></div>
+
+    <div class="elfsight-app-534898b9-490f-45b7-a0e6-7ec7d2ffca55"></div>
+    <Helmet>  
+          <script src="https://apps.elfsight.com/p/platform.js" defer></script>
+    </Helmet>
 <Helmet>
 <script async type="text/javascript" charset="utf-8" src="https://world-weather.info/wwinformer.php?userid=3fb7eb4b8c29bbe54ec5c070c0983fd1"></script>
+
 </Helmet>
 </div>
     )
