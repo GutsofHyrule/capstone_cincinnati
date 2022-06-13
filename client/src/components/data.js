1e12ac1aafc0e12b04e37b4887cad459689eef23
Spencer <dresmanns@gmail.com>
2022-06-13 10:56:54 -0400

    pulling

--- client/src/components/data.js
+++ client/src/components/data.js
@@ -1,5 +1,50 @@
-// const data = {
-//     cardData:[
+// import AmusesList from "./Places";
 
-//     ]
-// }
+
+const data = {
+    cardData:[
+{id:"629fa6634b25260c24997f59",
+name:"Beerfest",
+description:"Over 150 local breweries come together to celebrate Cincinnati's long ...",
+price:"$$",
+imageURL:"https://github.com/GutsofHyrule/capstone_cincinnati/blob/master/client..."},
+
+{id:"629fa6be4b25260c24997f5a",
+name:"Cincinnati Comic Expo",
+description:"A three day event located in the Duke Energy center. Come see cosplays...",
+price:"$$-$$$$",
+imageURL:"https://github.com/GutsofHyrule/capstone_cincinnati/blob/master/client..."},
+
+{id:"629fa70f4b25260c24997f5b",
+name:"Glow",
+description:"Put together by the Cincinnati circus, it is an event where you can se...",
+price:"$$",
+imageURL:"https://github.com/GutsofHyrule/capstone_cincinnati/blob/master/client..."},
+
+{id:"629fa75a4b25260c24997f5c",
+name:"Bunberry",
+description:"A three day music fest where artists from around the world come to pla...",
+price:"$$-$$$",
+imageURL:"https://github.com/GutsofHyrule/capstone_cincinnati/blob/master/client..."},
+
+{id:"629fa7bf4b25260c24997f5d",
+name:"Oktoberfest",
+description:"Oktoberfest is an old German tradition that we have adopted over here ...",
+price:"$$",
+imageURL:"https://github.com/GutsofHyrule/capstone_cincinnati/blob/master/client..."},
+
+{id:"629fa8914b25260c24997f5e",
+name:"Taste of Cincinnati",
+description:"An event that takes place every memorial day weekend. It includes food...",
+price:"$$",
+imageURL:"https://github.com/GutsofHyrule/capstone_cincinnati/blob/master/client..."},
+
+{id:"62a1ff5effe8470a1000dcc9",
+name:"Flying Pig",
+description:"A 26.2 mile run for charity that goes around the city. ",
+price:"FREE",
+imageURL:"https://github.com/GutsofHyrule/capstone_cincinnati/blob/master/client..."}
+
+    ]
+}
+export default data
