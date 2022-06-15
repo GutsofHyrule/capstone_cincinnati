f767f4e8f5116666cbdb58e7fb5e191dd3408260
Spencer <dresmanns@gmail.com>
2022-06-15 08:55:16 -0400

    pulling

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -9,9 +9,9 @@ const Navbar = () => {
         <nav className='nav-wrapper navbar navbar-expand-lg '>
             <div className="container-fluid " id='navbar'>
            <img className='cincy-logo left' src={Logo} alt=""></img>
-           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-           <span className="navbar-toggler-icon"></span>
-            </button>
+           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="/navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+        </button>
             <div className="collapse navbar-collapse" id="navbarNav">
                 <ul className='navbar-nav'>
                     <li className='right1 nav-item'><Link className="links nav-link"to='/'>Home</Link></li>
